5794cbe8930eee397f4e722d53d74d6df3ecd8d0
DhanashreeBalaji <dhanashreeblj@gmail.com>
2024-10-02 23:06:18 +0530

    Practicing basic paths

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-console.log("Starting a new project")
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Server is successfully running on port 3000");
+});
+
+// app.use((req,res) => {
+//     res.send("Hello from server at port 3000")
+// });
+
+// app.use("/", (req,res) => {
+//     res.send("Hello from / path")
+// })
+
+app.use("/hello", (req,res) => {
+    res.send("Hello from /hello")
+})
+
+app.use("/test", (req,res) => {
+    res.send("Hello from /test")
+})
+
+app.use("/", (req,res) => {
+    res.send("Hello from / path")
+     })
