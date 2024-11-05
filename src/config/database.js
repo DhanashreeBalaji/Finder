eb3d0faf423d10cdb5b11de02b72e36a9a88bb22
DhanashreeBalaji <dhanashreeblj@gmail.com>
2024-11-05 07:30:06 +0530

    Adding env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,13 @@
 // Here we write the code to connect to the database from our app using Mongoose library
 
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 // When this function is called, the db will connect to our app
 const connectDB =  async () => {
     await mongoose.connect(
-        "mongodb+srv://dhanashreeblj:JPEObLFs8xKqQmny@cluster0.3yvbzhr.mongodb.net/DevTinder"
+       process.env.MONGODB_URL
     );
-    
 };
 
 module.exports = connectDB;
